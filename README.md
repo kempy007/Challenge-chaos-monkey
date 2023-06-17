b0bba0e5016831ee20ffd95226e661762e8dd8c5
Kemp <13536174+kempy007@users.noreply.github.com>
2023-06-17 13:10:27 +0100

    Feature001 (#1)
    Merge work into Main for GHA

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # Challenge-chaos-monkey
-Challenge to build a chaos monkey service
+Challenge is to build a chaos monkey service as per the [Challenge.md](./Challenge.md) I moved how I defined how I would go about this task to this readme below.
+
+## Direction
+
+Operator-sdk satisfies lang=golang, dockerfile, operator manifests and deployment options.
+
+- [x] Use Operator-sdk to create CRD to configure a namespace else use all NS and to set a schedule (cron type). 
+- [x] Operator watches for this resource and creates/destroys a chaos Pod(perhaps use go concurrency) as per CRUD Operation with CRD options. 
+- [x] Filter out NS kube-system, monitoring
+- [x] Create manifests to setup workloads NS and introduce deployments and static pods.
+- [x] Create manifest for CRD for operator for workloads demo.
+- [x] Provide instructions in readme of howto deploy this operator and setup the demo.
+
+I did have starcoder enabled in my IDE, and I used starchat to ask a question to get unstuck as I have not been on this bicycle for a few years. I feel I need to be on the bicycle at least weekly to remain speedy,current and fresh.
+
+I kept my dev notes in the [BuildNotes.md](./BuildNotes.md) file.
+
+I have overun on my time but met the core requirements at the expense of writing the unittests thus functions maybe leaning towards monolith.
+
+## setup_and_prerequisites
+
+### MacOS
+
+- brew install operator-sdk kubebuilder kustomize
+
+## quick steps
+
+This will run the operator in developer mode from your IDE, external to your cluster.
+
+- git clone git@github.com:kempy007/Challenge-chaos-monkey.git
+- cd Challenge-chaos-monkey
+- kubectl apply -f demo-manifests/workloads.yaml
+- kubectl apply -f demo-manifests/crd.ChaosMonkey.yaml
+- make install run
+
+Watch pods be deleted in workloads namespace. You write your own version of this CRD demo-manifests/crd.ChaosMonkey.yaml to config alternate schedule, this is how the service is will be configured in production.
+
+
+## OLM Bundle install
+
+
+
+## My Versions
+
+operator-sdk version: "v1.29.0", commit: "78c564319585c0c348d1d7d9bbfeed1098fab006", kubernetes version: "v1.26.0", go version: "go1.20.4", GOOS: "darwin", GOARCH: "arm64"
+
+WARNING: This version information is deprecated and will be replaced with the output from kubectl version --short.  Use --output=yaml|json to get the full version.
+Client Version: version.Info{Major:"1", Minor:"27", GitVersion:"v1.27.1", GitCommit:"4c9411232e10168d7b050c49a1b59f6df9d7ea4b", GitTreeState:"clean", BuildDate:"2023-04-14T13:14:41Z", GoVersion:"go1.20.3", Compiler:"gc", Platform:"darwin/arm64"}
+Kustomize Version: v5.0.1
+Server Version: version.Info{Major:"1", Minor:"26", GitVersion:"v1.26.4+k3s1", GitCommit:"8d0255af07e95b841952563253d27b0d10bd72f0", GitTreeState:"clean", BuildDate:"2023-04-20T00:33:18Z", GoVersion:"go1.19.8", Compiler:"gc", Platform:"linux/amd64"}
---
+++ internal/controller/chaosmonkey_controller.go
@@ -0,0 +1,176 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	corev1 "github.com/kempy007/Challenge-chaos-monkey/api/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// ChaosMonkeyReconciler reconciles a ChaosMonkey object
+type ChaosMonkeyReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=core.cypherpunk.io,resources=chaosmonkeys,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core.cypherpunk.io,resources=chaosmonkeys/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=core.cypherpunk.io,resources=chaosmonkeys/finalizers,verbs=update
+
+// +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=pods/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=core,resources=pods/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the ChaosMonkey object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
+func (r *ChaosMonkeyReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = log.FromContext(ctx)
+
+	// TODO(user): your logic here
+	//crd := corev1.ChaosMonkeySpec{} 	//ERROR: does not implement client.Object (missing method DeepCopyObject)
+	var crd corev1.ChaosMonkey
+
+	err := r.Client.Get(ctx, req.NamespacedName, &crd)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			// TODO: Kill/Ground our channel here
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	NS := crd.Spec.Namespace
+	SCHED := crd.Spec.Schedule
+	TIMER := crd.Spec.Timer
+	STRTIM := fmt.Sprintf("%d", TIMER)
+
+	if NS == "" {
+		log.Log.Info("CRD has no namespace set")
+	}
+	if SCHED == "" {
+		log.Log.Info("CRD has no schedule set")
+	}
+	if TIMER == 0 {
+		log.Log.Info("CRD has no timer set")
+	}
+
+	log.Log.Info("ChaosMonkey named: " + crd.Name + " has namespace: " + NS + ", schedule: " + SCHED + ", timer: " + STRTIM)
+
+	//TODO: Add Channel Routine to execute scheduled actions
+	runTimer(NS, TIMER)
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *ChaosMonkeyReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&corev1.ChaosMonkey{}).
+		Complete(r)
+}
+
+// Timer Routine from Namespace, kill random pod in namespace
+func runTimer(NS string, T int) {
+	if NS == "kube-system" {
+		return
+	}
+	if NS == "monitoring" {
+		return
+	}
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		log.Log.Info(err.Error())
+		config, err = ctrl.GetConfig()
+		if err != nil {
+			log.Log.Info(err.Error())
+		}
+	}
+	if config == nil {
+		log.Log.Info("No config")
+		return
+	}
+	// Get all pods in namespace
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Log.Info(err.Error())
+	}
+	if clientSet == nil {
+		log.Log.Info("No clientSet")
+		return
+	}
+	podClient := clientSet.CoreV1().Pods(NS)
+	pods, err := podClient.List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		log.Log.Info(err.Error())
+	}
+	if pods == nil {
+		log.Log.Info("No pods")
+		return
+	}
+
+	var hitlist []string
+
+	for _, pod := range pods.Items {
+		hitlist = append(hitlist, pod.Name)
+		log.Log.Info("Hitlist: " + pod.Name)
+	}
+
+	for i := 0; i < 4; i++ {
+		// randomise order of hitlist
+		for i := len(hitlist) - 1; i > 0; i-- {
+			j := rand.Intn(i + 1)
+			hitlist[i], hitlist[j] = hitlist[j], hitlist[i]
+		}
+	}
+
+	for _, pod := range hitlist {
+		log.Log.Info("RND_Hitlist: " + pod)
+	}
+
+	for len(hitlist) > 0 {
+		log.Log.Info("Waiting for next schedule")
+		time.Sleep(time.Duration(T) * time.Minute)
+		log.Log.Info("Killing pod: " + hitlist[0])
+		err = podClient.Delete(context.TODO(), hitlist[0], metav1.DeleteOptions{})
+		if err != nil {
+			log.Log.Info(err.Error())
+		} else {
+			hitlist = hitlist[1:]
+		}
+	}
+	log.Log.Info("Chaos Schedule Completed")
+}
